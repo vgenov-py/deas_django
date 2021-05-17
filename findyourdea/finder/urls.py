034453cf865ecebb9f78aee2d324511b2b48b73c
unknown <genovese.work@gmail.com>
2021-05-17 10:06:01 +0200

    17052021

--- findyourdea/finder/urls.py
+++ findyourdea/finder/urls.py
@@ -3,6 +3,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("dea", views.index, name="index"),
+    path("todoslosdeas", views.todoslosdeas, name = "todoslosdeas"),
     path("comments/", views.comments, name="comments")
 ]
--- findyourdea/finder/views.py
+++ findyourdea/finder/views.py
@@ -6,9 +6,11 @@ from .queries import *
 
 def index(request):
     result, dea_latlng = get_nearest_dea(40.4240319, -3.6724659, Dea.objects.all())
-    print("KUGA ::>",result.x_utm)
     context = {"dea": result, "anchor_tag": f"https://www.google.com/maps/search/?api=1&query={dea_latlng[0]},{dea_latlng[1]}"}
     return render(request, "finder/index.html", context)
+
+def todoslosdeas(request):
+    return render(request, "finder/todoslosdeas.html")
 
 def comments(request):
     return HttpResponse("Esta en la sección de comentarios")
